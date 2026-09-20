9c6ff2a1a0469e1f39025011a2cead3239820de1
lkonlinetechshop <lk.online.techshop@gmail.com>
2026-09-20 12:17:40 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,28 +1,28 @@
+function searchProducts() {
 
-const searchInput = document.getElementById("search");
-
-searchInput.addEventListener("keyup", () => {
+    const searchInput = document.getElementById("search");
     const value = searchInput.value.toLowerCase();
-    let firstMatch = null;
 
-    document.querySelectorAll(".card").forEach(card => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
         const title = card.querySelector("h3").textContent.toLowerCase();
 
         if (title.includes(value)) {
             card.style.display = "block";
-
-            if (!firstMatch) {
-                firstMatch = card;
-            }
         } else {
             card.style.display = "none";
         }
+
     });
 
-    if (firstMatch && value !== "") {
-        firstMatch.scrollIntoView({
+    if (value !== "") {
+        document.getElementById("product-list").scrollIntoView({
             behavior: "smooth",
-            block: "center"
+            block: "start"
         });
     }
-});
+}
+
+document.getElementById("search").addEventListener("keyup", searchProducts);

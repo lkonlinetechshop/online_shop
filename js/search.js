137c618b13cadbc7ae12481b5c91609c5dada7b3
lkonlinetechshop <lk.online.techshop@gmail.com>
2026-09-20 12:11:49 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,20 +1,28 @@
+
 const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("keyup", () => {
-
     const value = searchInput.value.toLowerCase();
+    let firstMatch = null;
 
     document.querySelectorAll(".card").forEach(card => {
+        const title = card.querySelector("h3").textContent.toLowerCase();
 
-        const title =
-        card.querySelector("h3").textContent.toLowerCase();
-
-        if(title.includes(value)){
+        if (title.includes(value)) {
             card.style.display = "block";
+
+            if (!firstMatch) {
+                firstMatch = card;
+            }
         } else {
             card.style.display = "none";
         }
-
     });
 
+    if (firstMatch && value !== "") {
+        firstMatch.scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        });
+    }
 });

2a774a052b3693402fbee33dafcfd889519ea80d
lkonlinetechshop <lk.online.techshop@gmail.com>
2026-09-21 09:43:29 +0530

    Update product-details.js

--- js/product-details.js
+++ js/product-details.js
@@ -11,7 +11,7 @@ const products = [
 
         image: "images/2kw-220v-rituo-vfd-inverter-variable-frequency-drive.jpg",
         manualPdf: "manuals/2.2kw-220v-rituo-vfd-manual.pdf",
-        manualPdf: "DB620 Manual.pdf",
+        manualPdf: "manuals/DB620 Manual.pdf",
         
         size: "",
 
